74288a87b60f4ae8898ef51512d3c230c4bed719
Travis Lee <Leet3079@github.com>
2017-12-21 23:43:26 -0500

    added info for periods in schedule

--- app/src/main/java/com/strobertchs/edussentials/Schedule.java
+++ app/src/main/java/com/strobertchs/edussentials/Schedule.java
@@ -1,8 +1,12 @@
 package com.strobertchs.edussentials;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ListView;
+import android.widget.AdapterView.OnItemClickListener
 
 public class Schedule extends AppCompatActivity {
     String[] periodArray = {"Computer Science","Physics","Morality","Business Leadership"};
@@ -18,5 +22,17 @@ public class Schedule extends AppCompatActivity {
         CustomListAdapter array1 = new CustomListAdapter(this, periodArray);
         listView = (ListView) findViewById(R.id.ListViewID);
         listView.setAdapter(array1);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position,
+                                    long id) {
+                Intent intent = new Intent(Schedule.this, DetailActivity.class);
+                String message = periodArray[position];
+                intent.putExtra("info", message);
+                startActivity(intent);
+
+            }
+        });
     }
 }
