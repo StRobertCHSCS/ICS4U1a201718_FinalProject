aba3ca4ae537264d124afcb0a1b6880e163c9b0f
Travis Lee <Leet3079@github.com>
2017-12-21 21:52:03 -0500

    fixed rows in schedule, started custom adapter class

---
+++ app/src/main/java/com/strobertchs/edussentials/CustomListAdapter.java
@@ -0,0 +1,10 @@
+package com.strobertchs.edussentials;
+
+import android.widget.ArrayAdapter;
+
+/**
+ * Created by Travis on 2017-12-21.
+ */
+
+public class CustomListAdapter extends ArrayAdapter {
+}
