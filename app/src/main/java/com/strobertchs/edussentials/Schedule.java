4240daa2c0839f63ee4659713df3e243d304cf17
Travis Lee <Leet3079@github.com>
2017-12-21 22:47:48 -0500

    finished schedule, need to add it to main screen

--- app/src/main/java/com/strobertchs/edussentials/Schedule.java
+++ app/src/main/java/com/strobertchs/edussentials/Schedule.java
@@ -2,12 +2,21 @@ package com.strobertchs.edussentials;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ListView;
 
 public class Schedule extends AppCompatActivity {
+    String[] periodArray = {"Computer Science","Physics","Morality","Business Leadership"};
+
+    ListView listView;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_schedule);
+
+        CustomListAdapter array1 = new CustomListAdapter(this, periodArray);
+        listView = (ListView) findViewById(R.id.ListViewID);
+        listView.setAdapter(array1);
     }
 }
