21559014df47ab4469cdf39633f885f9931d48e5
Diwei Guan <diwei89@hotmail.com>
2017-12-22 15:17:48 -0500

    Fix merge conflict

--- app/src/main/java/team_infinity/boundless_manifest/MainMenuActivity.java
+++ app/src/main/java/team_infinity/boundless_manifest/MainMenuActivity.java
@@ -7,34 +7,26 @@ import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-<<<<<<< HEAD
+
 import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.PopupWindow;
-=======
+
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageButton;
->>>>>>> d3ae3ef9f35e9b76247b26b7bac481fd3c807be3
+
 
 public class MainMenuActivity extends AppCompatActivity
 {
     ImageButton mode;
-    private PopupWindow pop1;
-
-<<<<<<< HEAD
-    @Override
-    protected void onCreate(Bundle savedInstanceState)
-    {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main_menu);
-
-        mode = (ImageButton) findViewById(R.id.imageButton_mode);
-        mode.setOnClickListener(new OnModeClickListener(this));
-=======
     ImageButton playGame;
+    private PopupWindow pop1;
     FragmentManager fm = getSupportFragmentManager();
+
+
+    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_menu);
@@ -48,7 +40,9 @@ public class MainMenuActivity extends AppCompatActivity
                 continueQ.show(fm, "123");
             }
         });
->>>>>>> d3ae3ef9f35e9b76247b26b7bac481fd3c807be3
+        mode = (ImageButton) findViewById(R.id.imageButton_mode);
+        mode.setOnClickListener(new OnModeClickListener(this));
+
     }
 
 
