07640139c568f6acd785a142d73e7b077cec309f
Julian Lau <julian.lau18@ycdsbk12.ca>
2017-12-21 21:34:46 -0500

    Added GameActivity screen and linked it

---
+++ app/src/main/java/com/strobertchs/final_project/GameActivity.java
@@ -0,0 +1,46 @@
+package com.strobertchs.final_project;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.Random;
+
+public class GameActivity extends AppCompatActivity implements View.OnClickListener {
+
+//    int correctAnswer;
+//    Button buttonObjectChoice1;
+//    Button buttonObjectChoice2;
+//    Button buttonObjectChoice3;
+//    TextView textObjectPartA;
+//    TextView textObjectPartB;
+//    TextView textObjectScore;
+//    TextView textObjectLevel;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_game);
+
+//        textObjectPartA = (TextView)findViewById(R.id.textPartA);
+//        textObjectPartB = (TextView)findViewById(R.id.textPartB);
+//        textObjectScore = (TextView)findViewById(R.id.textScore);
+//        textObjectLevel = (TextView)findViewById(R.id.textLevel);
+//        buttonObjectChoice1 = (Button)findViewById(R.id.buttonChoice1);
+//        buttonObjectChoice2 = (Button)findViewById(R.id.buttonChoice2);
+//        buttonObjectChoice3 = (Button)findViewById(R.id.buttonChoice3);
+//
+//        buttonObjectChoice1.setOnClickListener(this);
+//        buttonObjectChoice2.setOnClickListener(this);
+//        buttonObjectChoice3.setOnClickListener(this);
+
+    }//onCreate ends here
+
+    @Override
+    public void onClick(View view) {
+
+    }
+}
--- app/src/main/java/com/strobertchs/final_project/MainActivity.java
+++ app/src/main/java/com/strobertchs/final_project/MainActivity.java
@@ -1,13 +1,28 @@
 package com.strobertchs.final_project;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+
+    @Override
+    public void onClick(View view) {
+        Intent i;
+        i = new Intent(this, GameActivity.class);
+        startActivity(i);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        setContentView(R.layout.activity_main);
+        Button buttonPlay = (Button)findViewById(R.id.buttonPlay);
+        buttonPlay.setOnClickListener(this);
+
     }
 }
