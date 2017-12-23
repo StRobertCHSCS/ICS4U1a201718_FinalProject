0a55711d89ded851263130e20e86ad8e7a5fceb4
Julian Lau <julian.lau18@ycdsbk12.ca>
2017-12-22 21:18:17 -0500

    Added setText() which updates the current bars

--- app/src/main/java/com/strobertchs/final_project/GameActivity.java
+++ app/src/main/java/com/strobertchs/final_project/GameActivity.java
@@ -5,7 +5,6 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import java.util.Random;
 
@@ -19,18 +18,29 @@ public class GameActivity extends AppCompatActivity implements View.OnClickListe
     Button buttonObjectYes;
     Button buttonObjectNo;
 
+    //Initializing Student
+    Student gabe = new Student("Gabe-kun", 1, true);
+
+    //Linking Student to the Event
+    Turn randomTurn = new Turn(gabe);
+
+    //testing
+    // randomTurn.makeTurn();
+
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
 
-        textObjectMoney = (TextView)findViewById(R.id.textMoney);
-        textObjectSocial = (TextView)findViewById(R.id.textSocial);
-        textObjectGrades = (TextView)findViewById(R.id.textGrades);
-        textObjectSleep = (TextView)findViewById(R.id.textSleep);
-        textObjectEvent = (TextView)findViewById(R.id.textEvent);
-        buttonObjectYes = (Button)findViewById(R.id.buttonYes);
-        buttonObjectNo = (Button)findViewById(R.id.buttonNo);
+        textObjectMoney = (TextView) findViewById(R.id.textMoney);
+        textObjectSocial = (TextView) findViewById(R.id.textSocial);
+        textObjectGrades = (TextView) findViewById(R.id.textGrades);
+        textObjectSleep = (TextView) findViewById(R.id.textSleep);
+        textObjectEvent = (TextView) findViewById(R.id.textEvent);
+        buttonObjectYes = (Button) findViewById(R.id.buttonYes);
+        buttonObjectNo = (Button) findViewById(R.id.buttonNo);
 
         buttonObjectYes.setOnClickListener(this);
         buttonObjectNo.setOnClickListener(this);
@@ -39,6 +49,29 @@ public class GameActivity extends AppCompatActivity implements View.OnClickListe
 
     @Override
     public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.buttonYes:
+//                //initialize a new int with the value contained in buttonObjectChoice1
+//                //Remember we put it there ourselves previously
+//                answerGiven = Integer.parseInt("" + buttonObjectChoice1.getText());
+//                break;
+//
+//            case R.id.buttonChoice2:
+//                answerGiven = Integer.parseInt("" + buttonObjectChoice2.getText());
+//                break;
+//
+//            case R.id.buttonChoice3:
+//                answerGiven = Integer.parseInt("" + buttonObjectChoice3.getText());
+//                break;
+//        }
+//        updateScoreAndLevel(answerGiven);
+//        setQuestion();
+    }
 
+    void setText() {
+        textObjectMoney.setText(gabe.getMoney());
+        textObjectSocial.setText(gabe.getSocial());
+        textObjectGrades.setText(gabe.getGrades());
+        textObjectSleep.setText(gabe.getSleep());
     }
 }
