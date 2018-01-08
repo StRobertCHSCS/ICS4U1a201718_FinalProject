af09fd0cd89ffdba0da707b05e70067d41b3b089
unknown <gavin74productions@gmail.com>
2018-01-08 18:19:11 -0500

    Added a personal toolbar and reconstructed (partially) the menu

--- app/src/main/java/com/strobertchs/frcscoutingapp/MainActivity.java
+++ app/src/main/java/com/strobertchs/frcscoutingapp/MainActivity.java
@@ -1,38 +1,48 @@
 package com.strobertchs.frcscoutingapp;
 
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
 
 
 public class MainActivity extends AppCompatActivity {
 
+    //Initializes the Drawer Variables and Toolbar
+    private Toolbar toolbar;
     private DrawerLayout mDrawerLayout;
-    private ActionBarDrawerToggle mToggle;
+    private NavigationView navDrawer;
+
+    private ActionBarDrawerToggle drawerToggle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        //Sets up the actionbar
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        // Sets up the Hamburger Menu
         mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
-        mToggle = new ActionBarDrawerToggle(this, mDrawerLayout, R.string.open, R.string.close);
-
-        mDrawerLayout.addDrawerListener(mToggle);
-        mToggle.syncState();
-
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        if(mToggle.onOptionsItemSelected(item)){
-            return true;
+        //Opens/closes the menu
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                mDrawerLayout.openDrawer(GravityCompat.START);
+                return true;
         }
         return super.onOptionsItemSelected(item);
     }
+
+
 
 }
