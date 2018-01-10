8039701342ac752b3046a6ce657ccddd0856c8a2
Travis Lee <Leet3079@github.com>
2018-01-09 19:25:16 -0500

    Added, and will continue to work on login screen and button

---
+++ app/src/main/java/com/strobertchs/edussentials/Login.java
@@ -0,0 +1,29 @@
+package com.strobertchs.edussentials;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+public class LoginActivity extends AppCompatActivity {
+
+    public Button signIn;
+
+    public void logIn() {
+        signIn = (Button)findViewById(R.id.signin);
+        signIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent login = new Intent(LoginActivity.this,GradeTracker.class);
+            }
+        });
+    }
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        logIn();
+    }
+}
