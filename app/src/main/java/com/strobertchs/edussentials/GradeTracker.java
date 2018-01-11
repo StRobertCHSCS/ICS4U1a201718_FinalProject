79795afada547da0d889109f39c5ece2245df3bb
Daniel <daniel.cheung18@ycdsbk12.ca>
2018-01-10 23:06:09 -0500

    Sign in now goes to menu

--- app/src/main/java/com/strobertchs/edussentials/GradeTracker.java
+++ app/src/main/java/com/strobertchs/edussentials/GradeTracker.java
@@ -12,7 +12,5 @@ public class GradeTracker extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_grade_tracker);
-        Intent menuIntent = new Intent(this, Schedule.class);
-        startActivity(menuIntent);
     }
 }
--- app/src/main/java/com/strobertchs/edussentials/Login.java
+++ app/src/main/java/com/strobertchs/edussentials/Login.java
@@ -9,21 +9,24 @@ import android.widget.Button;
 
 public class Login extends AppCompatActivity {
 
-    public Button signIn;
+    private Button log_in;
 
-    public void logIn() {
-        signIn = (Button)findViewById(R.id.signin);
-        signIn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent login = new Intent(Login.this,GradeTracker.class);
-            }
-        });
-    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-        logIn();
+
+        log_in = (Button) findViewById(R.id.signin);
+        log_in.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openMenu();
+            }
+        });
+    }
+
+    public void openMenu() {
+        Intent intent = new Intent(this, GradeTracker.class);
+        startActivity(intent);
     }
 }
