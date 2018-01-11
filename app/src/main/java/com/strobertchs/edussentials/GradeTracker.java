3ef5c79f849c325a2e55f156c1b1f3f86f28dfb5
Daniel <daniel.cheung18@ycdsbk12.ca>
2018-01-10 23:12:05 -0500

    Schedule button now functional

--- app/src/main/java/com/strobertchs/edussentials/GradeTracker.java
+++ app/src/main/java/com/strobertchs/edussentials/GradeTracker.java
@@ -8,9 +8,24 @@ import android.widget.Button;
 
 public class GradeTracker extends AppCompatActivity {
 
+    private Button scheduleOpen;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_grade_tracker);
+
+        scheduleOpen = (Button) findViewById(R.id.Schedule);
+        scheduleOpen.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openSchedule();
+            }
+        });
+    }
+
+    public void openSchedule() {
+        Intent intent = new Intent(this, Schedule.class);
+        startActivity(intent);
     }
 }
