e66945c049c02748deac3e1df0a1b088a646dfa5
unknown <gavin74productions@gmail.com>
2018-01-11 19:30:06 -0500

    Changed up the first portion of the scouting sheet

--- app/src/main/java/com/strobertchs/frcscoutingapp/MainActivity.java
+++ app/src/main/java/com/strobertchs/frcscoutingapp/MainActivity.java
@@ -63,13 +63,13 @@ public class MainActivity extends AppCompatActivity {
         Class fragmentClass = null;
 
         Log.d("ADebugTag", "Value:" + menuItem.getItemId());
-        if(menuItem.getItemId() == 2131230817){
+        if(menuItem.getItemId() == 2131230820){
             fragmentClass = HomeScreen.class;
         }
-        else if(menuItem.getItemId() == 2131230839) {
+        else if(menuItem.getItemId() == 2131230842) {
             fragmentClass = FieldScoutingSheet.class;
         }
-        else if(menuItem.getItemId() == 2131230840) {
+        else if(menuItem.getItemId() == 2131230843) {
             fragmentClass = PitScoutingSheet.class;
         }
         else{
