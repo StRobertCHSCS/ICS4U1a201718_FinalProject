c3dc46d44d0e0617251394492c44e71fbbeebea3
Jessica Lu <Jessica@Jessicas-MacBook-Pro.local>
2018-01-23 17:35:42 -0500

    brick sounds

--- core/src/com/ethanmajidi/javagame/Sprites/Brick.java
+++ core/src/com/ethanmajidi/javagame/Sprites/Brick.java
@@ -2,6 +2,7 @@ package com.ethanmajidi.javagame.Sprites;
 
 
 
+import com.badlogic.gdx.audio.Sound;
 import com.badlogic.gdx.maps.tiled.TiledMap;
 import com.badlogic.gdx.math.Rectangle;
 import com.badlogic.gdx.physics.box2d.World;
@@ -34,5 +35,8 @@ public class Brick extends InteractiveTileObject {
 
         //score
         Hud.addScore(200);
+
+        //sounds
+        JavaGame.manager.get("audio/sounds/BreakBlock.wav", Sound.class);
     }
 }
