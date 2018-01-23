9644d3bbead1300f0245a8ce82376a2ec5f70898
Ethan Majidi <ethanmajidi>
2018-01-23 01:09:58 -0500

    animations for running jumping falling

--- core/src/com/ethanmajidi/javagame/Sprites/Java.java
+++ core/src/com/ethanmajidi/javagame/Sprites/Java.java
@@ -1,5 +1,6 @@
 package com.ethanmajidi.javagame.Sprites;
 
+import com.badlogic.gdx.graphics.g2d.Animation;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.physics.box2d.Body;
@@ -7,6 +8,7 @@ import com.badlogic.gdx.physics.box2d.BodyDef;
 import com.badlogic.gdx.physics.box2d.CircleShape;
 import com.badlogic.gdx.physics.box2d.FixtureDef;
 import com.badlogic.gdx.physics.box2d.World;
+import com.badlogic.gdx.utils.Array;
 import com.ethanmajidi.javagame.JavaGame;
 import com.ethanmajidi.javagame.Screens.PlayScreen;
 
@@ -15,33 +17,102 @@ import com.ethanmajidi.javagame.Screens.PlayScreen;
  */
 
 public class Java extends Sprite {
+    public enum State { FALLING, JUMPING, STANDING, RUNNING};
+    public State currentState;
+    public State previousState;
     public World world;
     public Body b2body;
-
     private TextureRegion playerStand;
+    private Animation <TextureRegion> playerRun;
+    private Animation <TextureRegion> playerJump;
+    private float stateTimer;
+    private boolean runningRight;
 
     public Java(World world, PlayScreen screen){
         super(screen.getAtlas().findRegion("little_mario"));
         this.world = world;
+        currentState = State.STANDING;
+        previousState = State.STANDING;
+        stateTimer = 0;
+        runningRight = true;
+
+        Array<TextureRegion> frames = new Array<TextureRegion>();
+        for(int i = 1; i < 4; i++)
+            frames.add(new TextureRegion(getTexture(), i * 16, 0, 16, 16));
+        playerRun = new Animation<TextureRegion>(0.1f, frames);
+        frames.clear();
+
+        for(int i = 4; i < 6; i++)
+            frames.add(new TextureRegion(getTexture(), i * 16, 0, 16, 16));
+        playerJump = new Animation<TextureRegion>(0.1f, frames);
+
+        playerStand = new TextureRegion(getTexture(), 0, 0, 16, 16);
+
         defineMario();
-        playerStand = new TextureRegion(getTexture(), 0,0,16,16);
-        setBounds(0,0,16 / JavaGame.PPM,16 / JavaGame.PPM);
+        setBounds(0, 0, 16 / JavaGame.PPM, 16 / JavaGame.PPM);
         setRegion(playerStand);
+
     }
 
+
     public void update(float dt){
-        setPosition(b2body.getPosition().x - getWidth() /2, b2body.getPosition().y - getHeight()/2);
+        setPosition(b2body.getPosition().x - getWidth() / 2, b2body.getPosition().y - getHeight() / 2);
+        setRegion(getFrame(dt));
+    }
+
+    public TextureRegion getFrame(float dt){
+        currentState = getState();
+
+        TextureRegion region;
+        switch (currentState){
+            case JUMPING:
+                region = playerJump.getKeyFrame(stateTimer);
+                break;
+            case RUNNING:
+                region = playerRun.getKeyFrame(stateTimer, true);
+                break;
+            case FALLING:
+            case STANDING:
+            default:
+                region = playerStand;
+                break;
+        }
+
+        if((b2body.getLinearVelocity().x < 0 || !runningRight) && !region.isFlipX()){
+            region.flip(true, false);
+            runningRight = false;
+        }
+        else if((b2body.getLinearVelocity().x > 0 || runningRight) && region.isFlipX()){
+            region.flip(true, false);
+            runningRight = true;
+        }
+
+        stateTimer = currentState == previousState ? stateTimer + dt : 0;
+        previousState = currentState;
+        return region;
+
+    }
+
+    public State getState(){
+        if(b2body.getLinearVelocity().y > 0 || (b2body.getLinearVelocity().y < 0 && previousState == State.JUMPING))
+            return State.JUMPING;
+        else if(b2body.getLinearVelocity().y < 0)
+            return State.FALLING;
+        else if(b2body.getLinearVelocity().x != 0)
+            return State.RUNNING;
+        else
+            return State.STANDING;
     }
 
     public void defineMario(){
         BodyDef bdef = new BodyDef();
-        bdef.position.set(32 /JavaGame.PPM,32/JavaGame.PPM);
+        bdef.position.set(30 /JavaGame.PPM,30/JavaGame.PPM);
         bdef.type = BodyDef.BodyType.DynamicBody;
         b2body = world.createBody(bdef);
 
         FixtureDef fdef = new FixtureDef();
         CircleShape shape = new CircleShape();
-        shape.setRadius(5/ JavaGame.PPM);
+        shape.setRadius(6/ JavaGame.PPM);
 
         fdef.shape = shape;
         b2body.createFixture(fdef);
