bc82c154429c523fd049e253fcd58796dc1abf97
Julian Lau <julian.lau18@ycdsbk12.ca>
2018-01-23 22:07:17 -0500

    Fixed instructions to display correctly instead of error

--- app/src/main/java/com/strobertchs/final_project/InstructionsActivity.java
+++ app/src/main/java/com/strobertchs/final_project/InstructionsActivity.java
@@ -1,26 +1,25 @@
 package com.strobertchs.final_project;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.TextView;
+        import android.os.Bundle;
+        import android.support.v7.app.AppCompatActivity;
+        import android.view.View;
+        import android.widget.Button;
 
-public class GameOverActivity extends AppCompatActivity implements View.OnClickListener{
+public class InstructionsActivity extends AppCompatActivity implements View.OnClickListener {
 
     Button buttonObjectBack;
     Button buttonObjectPlay;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-            super.onCreate(savedInstanceState);
-            setContentView(R.layout.activity_instructions);
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_instructions);
 
-            buttonObjectBack = (Button)findViewById(R.id.buttonBack);
-            buttonObjectPlay = (Button)findViewById(R.id.buttonPlay);
-            buttonObjectBack.setOnClickListener(this);
-            buttonObjectPlay.setOnClickListener(this);
+        buttonObjectBack = (Button) findViewById(R.id.buttonBack);
+        buttonObjectPlay = (Button) findViewById(R.id.buttonPlay);
+        buttonObjectBack.setOnClickListener(this);
+        buttonObjectPlay.setOnClickListener(this);
     }
     @Override
     public void onClick(View view) {
