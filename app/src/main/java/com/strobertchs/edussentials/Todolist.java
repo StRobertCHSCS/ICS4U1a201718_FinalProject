8cea90a3e44bde1f039b50be1ad7cc4624809837
Daniel <daniel.cheung18@ycdsbk12.ca>
2018-01-24 00:36:29 -0500

    Home now connects to to do list

---
+++ app/src/main/java/com/strobertchs/edussentials/Todolist.java
@@ -0,0 +1,13 @@
+package com.strobertchs.edussentials;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Todolist extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_todolist);
+    }
+}
