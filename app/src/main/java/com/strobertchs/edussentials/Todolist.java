e26bf9a5753b62ccc96b7e207425722f62b099fd
Daniel <daniel.cheung18@ycdsbk12.ca>
2018-01-24 02:25:29 -0500

    Added to do list

--- app/src/main/java/com/strobertchs/edussentials/Todolist.java
+++ app/src/main/java/com/strobertchs/edussentials/Todolist.java
@@ -1,13 +1,44 @@
 package com.strobertchs.edussentials;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class Todolist extends AppCompatActivity {
+    private ArrayList<String> tasks;
+    private ArrayAdapter<String> tasksAdapter;
+    private ListView lvTasks;
+
+    private Button newTaskOpen;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_todolist);
+
+
+        lvTasks = (ListView)findViewById(R.id.lvTasks);
+        tasks = new ArrayList<>();
+        tasksAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, tasks);
+        lvTasks.setAdapter(tasksAdapter);
+        tasks.add("Task 1");
+        tasks.add("Task 2");
+
+    }
+
+
+    public void onAddTask(View v) {
+        EditText etNewTask = (EditText) findViewById(R.id.taskName);
+        String taskText = etNewTask.getText().toString();
+        tasksAdapter.add(taskText);
+        etNewTask.setText("");
+
     }
 }
