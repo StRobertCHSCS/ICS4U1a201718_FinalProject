dfd1f11cf7b23843d155de7886e54271074fd686
Ethan Majidi <ethanmajidi>
2018-01-25 16:17:02 -0500

    Goomba now disappears after being destoryed

--- core/src/com/ethanmajidi/javagame/Sprites/Goomba.java
+++ core/src/com/ethanmajidi/javagame/Sprites/Goomba.java
@@ -1,6 +1,7 @@
 package com.ethanmajidi.javagame.Sprites;
 
 import com.badlogic.gdx.graphics.g2d.Animation;
+import com.badlogic.gdx.graphics.g2d.Batch;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.physics.box2d.BodyDef;
@@ -79,6 +80,11 @@ public class Goomba extends Enemy {
         fdef.restitution = 0.5f;
         fdef.filter.categoryBits = JavaGame.ENEMY_HEAD_BIT;
         b2body.createFixture(fdef).setUserData(this);
+    }
+
+    public void draw(Batch batch){
+        if(!destoryed || stateTime < 1)
+            super.draw(batch);
     }
 
     @Override
