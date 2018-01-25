56f375397ea787ac94dc2498f5d324562d5a0759
Travis Lee <Leet3079@github.com>
2018-01-25 16:40:31 -0500

    added layout for dialog button of average calculator

--- app/src/main/java/com/strobertchs/edussentials/AverageCalculator.java
+++ app/src/main/java/com/strobertchs/edussentials/AverageCalculator.java
@@ -2,12 +2,31 @@ package com.strobertchs.edussentials;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class AverageCalculator extends AppCompatActivity {
+    private TextView textviewGradeinput;
+    private TextView textviewWeightFactor;
+    private Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_average_calculator);
+
+        textviewGradeinput = (TextView) findViewById(R.id.textview_gradeinput);
+        textviewWeightFactor = (TextView) findViewById(R.id.textview_weightfactor);
+        button = (Button) findViewById(R.id.dialogbutton);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openDialog();
+            }
+        });
+    }
+    public void openDialog(){
+
     }
 }
