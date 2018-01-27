40e01658b552df18b9d815c654956f89fbb0ccff
Gavin Li <gavin74productions@gmail.com>
2018-01-27 12:08:35 -0500

    Added button implementation in Field Scouting Sheet button

--- app/src/main/java/com/strobertchs/frcscoutingapp/FieldScoutingSheet.java
+++ app/src/main/java/com/strobertchs/frcscoutingapp/FieldScoutingSheet.java
@@ -1,25 +1,41 @@
 package com.strobertchs.frcscoutingapp;
 
-import android.content.Context;
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 
-public class FieldScoutingSheet extends Fragment {
-
-    public static Fragment newInstance(Context context) {
-        HomeScreen f = new HomeScreen();
-
-        return f;
-    }
+public class FieldScoutingSheet extends Fragment{
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstancestate){
-        ViewGroup root = (ViewGroup) inflater.inflate(R.layout.fragment_field_scouting_sheet, null);
-        return root;
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstancestate) {
+        ViewGroup view = (ViewGroup) inflater.inflate(R.layout.fragment_field_scouting_sheet, container, false);
+        Button button1 = view.findViewById(R.id.buttonNext1);
+        button1.setOnClickListener(new View.OnClickListener() {
+            @SuppressLint("ResourceType")
+            @Override
+            public void onClick(View view) {
+                Fragment fragment = new Fragment();
+                Class fragmentClass = null;
+                Log.d("ADebugTag", "Value:" + view.getId());
+                if(view.getId() == 2131230778) {
+                    fragmentClass = FieldScoutingSheet2.class;
+                }
+                try {
+                    fragment = (Fragment) fragmentClass.newInstance();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+                //Insert the fragment by replacing any existing fragment
+                FragmentManager fragmentManager = getFragmentManager();
+                fragmentManager.beginTransaction().replace(R.id.fragmentContent, fragment).commit();
+            }
+        });
+        return view;
+
     }
-
-
 }
--- app/src/main/java/com/strobertchs/frcscoutingapp/MainActivity.java
+++ app/src/main/java/com/strobertchs/frcscoutingapp/MainActivity.java
@@ -11,8 +11,6 @@ import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.Button;
 
 
 public class MainActivity extends AppCompatActivity {
