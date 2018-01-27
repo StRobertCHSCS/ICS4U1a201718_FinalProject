f1fc6e4bd7cc3bb139a5070a43225e5b22cd22a2
Diwei Guan <diwei89@hotmail.com>
2018-01-26 20:48:38 -0500

    Room functionality in place, trying to tie to SQL

---
+++ app/src/main/java/team_infinity/boundless_manifest/GameData.java
@@ -0,0 +1,18 @@
+package team_infinity.boundless_manifest;
+
+import android.arch.persistence.room.Entity;
+import android.arch.persistence.room.ForeignKey;
+import android.arch.persistence.room.PrimaryKey;
+
+/**
+ * Created by Vallyath on 2018-01-26.
+ */
+
+
+@Deprecated
+public class GameData {
+    @PrimaryKey
+    public String userName;
+
+
+}
---
+++ app/src/main/java/team_infinity/boundless_manifest/UserInfoDao.java
@@ -0,0 +1,17 @@
+package team_infinity.boundless_manifest;
+
+import android.arch.persistence.room.Dao;
+import android.arch.persistence.room.Query;
+
+import java.util.List;
+
+/**
+ * Created by Vallyath on 2018-01-26.
+ */
+
+@Dao
+public interface UserInfoDao {
+
+    //@Query("SELECT * FROM userInfo") working on something else atm
+
+}
