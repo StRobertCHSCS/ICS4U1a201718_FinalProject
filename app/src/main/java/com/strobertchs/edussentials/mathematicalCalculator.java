19fb7957e125bd0237cfb8c10f8c97fd338874f0
Travis Lee <Leet3079@github.com>
2018-01-27 00:28:19 -0500

    created java class for calculator

--- app/src/main/java/com/strobertchs/edussentials/mathematicalCalculator.java
+++ app/src/main/java/com/strobertchs/edussentials/mathematicalCalculator.java
@@ -10,4 +10,11 @@ public class mathematicalCalculator {
     private double current_mark;
     private double current_weight;
 
+    public mathematicalCalculator(){
+        grade = 0;
+        total_weight = 0;
+        current_mark = 0;
+        current_weight = 0;
+    }
+
 }
