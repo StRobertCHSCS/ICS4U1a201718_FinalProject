db0ff8b061ab8e86153da17ee85c7ad52f43a75b
Travis Lee <Leet3079@github.com>
2018-01-27 21:39:33 -0500

    added webview for calender

--- app/src/main/java/com/strobertchs/edussentials/Calendar.java
+++ app/src/main/java/com/strobertchs/edussentials/Calendar.java
@@ -2,6 +2,7 @@ package com.strobertchs.edussentials;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 
@@ -16,6 +17,18 @@ public class Calendar extends AppCompatActivity {
 
         webView = (WebView) findViewById(R.id.calendarWeb);
         webView.setWebViewClient(new WebViewClient());
-        webView.loadUrl("stro.ycdsb.ca/our-school/calendar");
+        webView.loadUrl("http://stro.ycdsb.ca/our-school/calendar%22");
+
+                WebSettings webSettings = webView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (webView.canGoBack()) {
+            webView.goBack();
+        } else {
+            super.onBackPressed();
+        }
     }
 }
