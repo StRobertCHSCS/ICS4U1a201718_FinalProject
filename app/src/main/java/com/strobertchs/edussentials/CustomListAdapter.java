0f211dcee4b4021544a44177cee2aa917d886112
Travis Lee <Leet3079@github.com>
2018-01-28 00:45:11 -0500

    changed times in schedule to display normal time

--- app/src/main/java/com/strobertchs/edussentials/CustomListAdapter.java
+++ app/src/main/java/com/strobertchs/edussentials/CustomListAdapter.java
@@ -43,6 +43,6 @@ public class CustomListAdapter extends ArrayAdapter {
 
         return rowView;
 
-    };
+    }
 
 }
--- app/src/main/java/com/strobertchs/edussentials/Schedule.java
+++ app/src/main/java/com/strobertchs/edussentials/Schedule.java
@@ -9,15 +9,18 @@ import android.widget.ListView;
 import android.widget.AdapterView.OnItemClickListener;
 
 import java.util.Arrays;
-import java.util.Scanner;
+
 
 public class Schedule extends AppCompatActivity {
     String[] periodArray = {"Period 1","Period 2","Period 3","Period 4","Lunch"};
-    String[] timeArray = {"Time Slot 1","Time Slot 2","Time Slot 3","Time Slot 4","LunchTime"};
+    String[] timeArray = {"8:30-9:51","9:53-11:09","(A- 11:53-1:13, B- 11:11-11:53+12:30-1:13, C-11:09-12:30 ","1:13-2:30","A- 11:09-11:53, B- 11:53-12:30, C-12:30-1:13"};
     ListView listView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_schedule);
+
         CustomListAdapter array1 = new CustomListAdapter(this, periodArray,timeArray);
         listView = (ListView) findViewById(R.id.ListViewID);
         listView.setAdapter(array1);
